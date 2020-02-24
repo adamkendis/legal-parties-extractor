8672077b6cb9630e8530d23d9e49487d6a0bebb1
Adam Kendis <atkendis@gmail.com>
2020-02-23 23:56:13 -0800

    Utilized app factory func and implemented shell context processor.

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-from partyparser import app
+from partyparser import create_app, db
+from partyparser.models import CourtCase
+
+app = create_app()
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'CourtCase': CourtCase}
