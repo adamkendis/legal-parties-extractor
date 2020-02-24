fb9d72023f0313db0438d5b11423370e16296c66
Adam Kendis <atkendis@gmail.com>
2020-02-24 00:23:54 -0800

    Fixed SQLAlchemy database uri.

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import environ, path
 
 class Config(object):
 
-    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL') or 'sqlite:////' \
+    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL') or 'sqlite:///' \
         + path.join(path.abspath(path.dirname(__file__)), 'court_cases.db')
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
