0bee8d1d1b77cb937419df312f33055f1ddaa3f1
Adam Kendis <atkendis@gmail.com>
2020-02-24 14:06:33 -0800

    Added TestConfig class. Set SQLALCHEMY_ECHO to False in base Config class.

--- config.py
+++ config.py
@@ -1,9 +1,17 @@
 from os import environ, path
 
 
+# Base Config class
 class Config(object):
-
+    # SQLite database created in project root dir
     SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL') or 'sqlite:///' \
         + path.join(path.abspath(path.dirname(__file__)), 'court_cases.db')
-    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class TestConfig(Config):
+
+    TESTING = True
+    # Use in-memory database for testing
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
