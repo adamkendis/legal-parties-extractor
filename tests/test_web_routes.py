4ef88389fbd2f5ef051d33c636780b1ac86bc915
Adam Kendis <atkendis@gmail.com>
2020-02-24 20:29:08 -0800

    Web/cases GET test, Added flask-testing and refactored tests.

--- tests/test_web_routes.py
+++ tests/test_web_routes.py
@@ -2,25 +2,35 @@
 import unittest
 
 # Third party imports
+from flask_testing import TestCase
+from sqlalchemy import exc
 
 # Local app imports
-from partyparser import create_app
+from partyparser import create_app, db
+from partyparser.models import CourtCase
 from config import TestConfig
 
 
-class WebInterfaceRoutesTests(unittest.TestCase):
+class WebInterfaceRoutesTests(TestCase):
     """Test suite for web interface endpoints"""
+
+    render_templates = False
+
+    def create_app(self):
+        return create_app(TestConfig)
 
     # Before each test method
     def setUp(self):
-        self.app = create_app(TestConfig)
-        self.app_context = self.app.app_context()
-        self.app_context.push()
-        self.client = self.app.test_client()
+        self.mock_data = [
+            {'id': 1, 'plaintiff': 'John Doe', 'defendant': 'Jane Doe'},
+            {'id': 2, 'plaintiff': 'Frank Zappa', 'defendant': 'Eric Clapton'}
+        ]
+        db.create_all()
 
     # After each test method
     def tearDown(self):
-        self.app_context.pop()
+        db.session.remove()
+        db.drop_all()
 
     def test_get_request_to_base_url(self):
         """Test sending GET request to base url returns 200 status code"""
@@ -36,6 +46,15 @@ class WebInterfaceRoutesTests(unittest.TestCase):
         """Test sending POST request to index returns 405 status code"""
         res = self.client.post('/index', data={})
         self.assertEqual(res.status_code, 405)
+
+    def test_get_to_web_cases(self):
+        """Test sending GET request to /web/cases returns all courtcases stored in db"""
+        res = self.client.get('/web/cases')
+        self.assertEqual(res.status_code, 200, 'GET to /web/cases should return 200 status code')
+        self.assert_template_used('index.html')
+        self.assertTrue('John Doe' and 'Jane Doe' in res.body)
+        self.assertTrue('Frank Zappa' and 'Eric Clapton' in res.body)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
