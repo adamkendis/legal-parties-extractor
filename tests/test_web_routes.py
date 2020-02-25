c8b12299cb604dd41de2d289cb573a49cb80d649
Adam Kendis <atkendis@gmail.com>
2020-02-24 21:12:51 -0800

    Added seed_db helper func and refactored test_get_to_web_cases

--- tests/test_web_routes.py
+++ tests/test_web_routes.py
@@ -3,7 +3,6 @@ import unittest
 
 # Third party imports
 from flask_testing import TestCase
-from sqlalchemy import exc
 
 # Local app imports
 from partyparser import create_app, db
@@ -21,10 +20,6 @@ class WebInterfaceRoutesTests(TestCase):
 
     # Before each test method
     def setUp(self):
-        self.mock_data = [
-            {'id': 1, 'plaintiff': 'John Doe', 'defendant': 'Jane Doe'},
-            {'id': 2, 'plaintiff': 'Frank Zappa', 'defendant': 'Eric Clapton'}
-        ]
         db.create_all()
 
     # After each test method
@@ -32,28 +27,41 @@ class WebInterfaceRoutesTests(TestCase):
         db.session.remove()
         db.drop_all()
 
+    # Helper method
+    def seed_db(self):
+        mock_data = [
+            CourtCase(plaintiff='John Doe', defendant='Jane Doe'),
+            CourtCase(plaintiff='Frank Zappa', defendant='Eric Clapton')
+        ]
+        db.session.add_all(mock_data)
+        db.session.commit()
+
     def test_get_request_to_base_url(self):
-        """Test sending GET request to base url returns 200 status code"""
+        """Test GET request to base url returns 200 status code"""
         res = self.client.get('/')
-        self.assertEqual(res.status_code, 200, 'GET to / should return 200 status code')
+        self.assertEqual(res.status_code, 200,
+                         'GET to / should return 200 status code')
 
     def test_get_request_to_index(self):
-        """Test sending GET request to index returns 200 status code"""
+        """Test GET request to index returns 200 status code"""
         res = self.client.get('/index')
-        self.assertEqual(res.status_code, 200, 'GET to / should return 200 status code')
+        self.assertEqual(res.status_code, 200,
+                         'GET to / should return 200 status code')
 
     def test_post_request_to_index(self):
-        """Test sending POST request to index returns 405 status code"""
+        """Test POST request to index returns 405 status code"""
         res = self.client.post('/index', data={})
         self.assertEqual(res.status_code, 405)
 
     def test_get_to_web_cases(self):
-        """Test sending GET request to /web/cases returns all courtcases stored in db"""
+        """Test GET to /web/cases returns all courtcases in context variable"""
+        self.seed_db()
+        db_cases = CourtCase.query.all()
         res = self.client.get('/web/cases')
-        self.assertEqual(res.status_code, 200, 'GET to /web/cases should return 200 status code')
+        self.assertEqual(res.status_code, 200,
+                         'GET to /web/cases should return 200 status code')
         self.assert_template_used('index.html')
-        self.assertTrue('John Doe' and 'Jane Doe' in res.body)
-        self.assertTrue('Frank Zappa' and 'Eric Clapton' in res.body)
+        self.assertEqual(self.get_context_variable('cases'), db_cases)
 
 
 if __name__ == '__main__':
