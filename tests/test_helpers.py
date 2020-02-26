ec9c099dbe6eb572f74e14572b8e78e3f8e335f9
Adam Kendis <atkendis@gmail.com>
2020-02-25 17:27:41 -0800

    Implemented failing test for format_case success.

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -21,6 +21,21 @@ class HelperFunctionsTests(TestCase):
     # Setup before each test method
     def setUp(self):
         self.test_file = 'test_file.xml'
+        # Test case as it will be returned from the db
+        self.test_case = {
+            'id': 1,
+            'plaintiff': 'John Doe',
+            'defendant': 'Jane Doe'
+        }
+        # Test case formatted for JSON response
+        self.formatted_test_case = {
+            'type': 'courtcase',
+            'id': 1,
+            'attributes': {
+                'plaintiff': 'John Doe',
+                'defendant': 'Jane Doe'
+            }
+        }
 
     # Cleanup after each test method
     def tearDown(self):
@@ -36,6 +51,22 @@ class HelperFunctionsTests(TestCase):
         result = verified_file_type('some_random_file.jpeg')
         self.assertFalse(result)
 
+    def test_format_case_success(self):
+        """
+        Return formatted case object.
+        Input: { id: <int>, plaintiff: <str>, defendant: <str> }
+        Output: {
+            type: 'courtcase',
+            id: <int>
+            attributes: {
+                plaintiff: <str>,
+                defendant: <str>
+            }
+        }
+        """
+        result = format_case(self.test_case)
+        self.assertEqual(result, self.formatted_test_case)
 
-if __name__ == "__main__"
+
+if __name__ == "__main__":
     unittest.main(verbosity=2)
