4b30764c23a2daf551b30c95b1f0090d6e5f74f0
Adam Kendis <atkendis@gmail.com>
2020-02-26 12:20:46 -0800

    remove_uploads helper func to clear uploaded files from uploads dir.

--- partyparser/helpers.py
+++ partyparser/helpers.py
@@ -1,3 +1,9 @@
+import os
+import shutil
+from time import sleep
+
+from flask import current_app
+
 def verified_file_type(filename):
     # Return bool indicating if filename is an allowed filetype
     valid_filetypes = set(['xml'])
@@ -14,3 +20,12 @@ def format_case(case):
             'defendant': case['defendant']
         }
     }
+
+
+def remove_uploads():
+    # Delete uploaded xml files in uploads dir
+    uploads_dir = current_app.config['UPLOAD_FOLDER']
+    if os.path.isdir(uploads_dir):
+        sleep(2)
+        shutil.rmtree(uploads_dir, ignore_errors=True)
+        os.mkdir(uploads_dir)
